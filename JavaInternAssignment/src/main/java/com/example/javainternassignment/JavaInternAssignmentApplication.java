95cc8c8a80160209ae30dc61c20998679f66bd60
samsriti <109064638+samsriti@users.noreply.github.com>
2023-07-20 14:30:34 +0545

    folder structure

---
+++ JavaInternAssignment/src/main/java/com/example/javainternassignment/JavaInternAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.javainternassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternAssignmentApplication.class, args);
+    }
+
+}
---
+++ JavaInternAssignment/src/test/java/com/example/javainternassignment/JavaInternAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javainternassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
